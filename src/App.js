4abd9c7d95567acc50a6f054d3a28c612fcc4280
Joseph Kertz <josephkertz@gmail.com>
2024-06-12 10:17:24 -0500

    create error state, add thank you message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import {useState} from 'react'
 import rcimg from './assets/images/illustration-sign-up-desktop.svg'
 import rcimgm from './assets/images/illustration-sign-up-mobile.svg'
 import chkmrk from './assets/images/icon-list.svg'
+import icnSuccess from './assets/images/icon-success.svg'
+
 function App() {
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  function submitForm(e){
+    e.preventDefault();
+    if(!validateEmail(email)){
+      setError(true);
+
+    } else{
+      setError(false);
+      setSuccess(true);
+    }
+
+  }
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false)
+
   return (
     <div className="background">
       <div className='page'>
+       {!success && <>
         <div className='left-column'>
          <div className='mobile-padding'>
           <h1 className='heading'>Stay Updated!</h1>
@@ -17,10 +44,13 @@ function App() {
               <CheckmarkItem text='Measuring to ensure updates are a successs'/>
               <CheckmarkItem text='And much more!'/>
             </div>
-            <small>Email address</small>
-            <form className='form'>
-              <input required type='email' placeholder='email@company.com'/>
-              <button type='submit'>Subscribe to monthly newsletter</button>
+            <div className='email-label'>
+              <small>Email address</small>
+              {error && <small className='error-email-label'>Valid email required</small>}
+            </div>
+            <form className='form' >
+              <input onChange={(e) => setEmail(e.target.value)} value={email} type='email' placeholder='email@company.com' className={`${error && 'error-email-input'}`}/>
+              <button onClick={(e)=> submitForm(e)}>Subscribe to monthly newsletter</button>
             </form>
          </div>
         </div>
@@ -30,10 +60,19 @@ function App() {
             <img src={rcimg} className='r-column-image'/>
           </picture>
         </div>
+        </>}
+       {success && <div className='success-page'>
+        <img src={icnSuccess} className='icnSuccess-img' />
+        <div className='heading success-heading'>Thanks for subscribing!</div>
+        <div className='success-subheading'>A confirmation email has been sent to <span style={{fontWeight: 700}}>{email}</span>. Please open it and click the button inside to confirm your subscription.</div>
+        <button className='dismiss-button' onClick={() => setSuccess(false)}>Dismiss message</button>
+       </div>}
       </div>
     </div>
   );
 }
+
+
 
 function CheckmarkItem({ text }){
   return(
